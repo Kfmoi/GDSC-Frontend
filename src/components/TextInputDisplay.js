574a1a55e0095dfb43efb24347f87c12a2f60f29
Hussain Muhammed <muhammeh@mcmaster.ca>
2024-02-03 14:16:23 -0500

    - created main text input display function component with some functions in it

--- src/components/TextInputDisplay.js
+++ src/components/TextInputDisplay.js
@@ -1,3 +1,14 @@
 // import useState, a React Hook, for components to manage the states
 import React, { useState } from "react";
 
+// main function 
+const TextInputDisplay = () => {
+
+    // useState function returns array with current state value and function to update it
+    const [text, setText] = useState('');
+
+    // function for backspace
+    const handleBackspace = () => {
+        setText(text.slice(0, text.length-1));
+    };
+}
