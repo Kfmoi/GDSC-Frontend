872a85572b01d61685eb7047e6c8a856c7e53854
Hussain Muhammed <muhammeh@mcmaster.ca>
2024-02-03 15:52:07 -0500

    - made functions for scrolling and selecting in picker

--- src/components/ScrollPicker.js
+++ src/components/ScrollPicker.js
@@ -6,16 +6,36 @@ const ScrollPicker = ({ onSelect }) => {
     // variable for storing alphabet
     const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-    // function for selecting letter
+    // state hook for the state of current letter in scroll picker
+    const [letterIdx, setLetterIdx] = useState(0);
+
+    // make functions for scrolling up, down, and selecting
+    const handleScrollUp = () => {
+        if (letter > 0) setLetterIdx(letterIdx-1);
+        else setLetterIdx(alphabet.length-1);
+    };
+
+    const handleScrollDown = () => {
+        if (letter < alphabet.length) setLetterIdx(letterIdx+1);
+        else setLetterIdx(0);
+    };
+
     const handleSelect = (letter) => {
-        onSelect(letter);
+        const currLetter = alphabet[letterIdx];
+        onSelect(currLetter);
     };
 
     return (
         <div>
-            
+            {/* maps each letter in alphabet list to letter selected */}
+           { alphabet.map((letter) => (
+                <button key = {letter} onClick = {() => handleSelect(letter)}>
+                    {letter}
+                </button>
+           ))} 
         </div>
     )
 
 };
 
+export default ScrollPicker;
