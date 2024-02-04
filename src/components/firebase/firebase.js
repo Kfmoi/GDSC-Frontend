891b0a7bb95774ed2a1115f050c694aa0083495f
Hussain Muhammed <muhammeh@mcmaster.ca>
2024-02-04 11:44:41 -0500

    - made firebase app and exported database

---
+++ src/components/firebase/firebase.js
@@ -0,0 +1,23 @@
+// firebase configuration document
+
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+
+// dictionary for firebase configuration attributes
+const firebaseConfig = {
+    apiKey: "AIzaSyDPVLFqJEHKiveDD9RoSIYQeO2ZpUmtEak",
+    authDomain: "typing-assistant-81810.firebaseapp.com",
+    projectId: "typing-assistant-81810",
+    storageBucket: "typing-assistant-81810.appspot.com",
+    messagingSenderId: "327729751390",
+    appId: "1:327729751390:web:f894291ed870c7869c9bcf",
+    measurementId: "G-X7EF3LP54D"
+  };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// get firebase database
+const db = firebase.firestore();
+
+export default db;
