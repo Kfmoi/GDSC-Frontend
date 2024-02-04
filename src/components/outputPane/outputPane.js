0cce8b6469e6481ceaf92dd0ecbb969bbfd158d0
Hussain Muhammed <muhammeh@mcmaster.ca>
2024-02-04 02:07:21 -0500

    put back outputpane file

---
+++ src/components/outputPane/outputPane.js
@@ -0,0 +1,22 @@
+
+import './OutputPane.css';
+
+const OutputPane = () => {
+
+
+    return (
+        <div className="outputBox">
+            <button className="clear">Clear all text</button>
+            <button className="copy">Copy to clipboard</button>
+            <textarea className="outputTextBox" type = "text" value="" 
+                readOnly placeholder='Output Text' rows={12} wrap='soft' cols={35}/>
+            <div className='backspace'>
+                <button className="back">Backspace</button>
+                <button className="super-back">Super Backspace</button>
+            </div>
+            <button className="copy">Space</button>
+        </div>
+    );
+};
+
+export default OutputPane;
