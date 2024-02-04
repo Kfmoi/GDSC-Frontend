1f915a05303982a82bf5f28f9b1004f62f9e2761
Mohammad Mahdi Mahboob <mahbom2@mcmaster.ca>
2024-02-04 04:23:23 -0500

    Add corrected functionalities for textarea buttons Added corrected functionalities based on Hussain's work.

--- src/components/outputPane/OutputPane.js
+++ src/components/outputPane/OutputPane.js
@@ -1,20 +1,56 @@
-
+import React, { useState } from "react";
 import './OutputPane.css';
 
 const OutputPane = ({ outputText }) => {
+    const [text, setText] = useState('');
 
+    const handleClear = () => {
+        setText('');
+    };
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text);
+    };
+
+    const handleBackspace = () => {
+        if (text.length === 0) {
+            return;
+        }
+        setText(text.slice(0, text.length-1));
+    };
+
+    const handleSuperBackspace = () => {
+        if (text.length === 0) {
+            return;
+        }
+        var alnum = ((text[text.length - 1] >= 'a' && text[text.length - 1] <= 'z') ||
+            (text[text.length - 1] >= 'A' && text[text.length - 1] <= 'Z') ||
+            (text[text.length - 1] >= '0' && text[text.length - 1] <= '9'));
+        for (let i = text.length - 1; i >= 0; i--) {
+            if ((alnum && !((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z') || (text[i] >= '0' && text[i] <= '9'))) ||
+                (!alnum && ((text[i] >= 'a' && text[i] <= 'z') || (text[i] >= 'A' && text[i] <= 'Z') || (text[i] >= '0' && text[i] <= '9')))) {
+                setText(text.slice(0, i + 1).trim());
+                return;
+            }
+        }
+        setText('');
+    };
+
+    const handleSpace = () => {
+        setText(text + ' ');
+    }
 
     return (
         <div className="outputBox">
-            <button className="clear">Clear all text</button>
-            <button className="copy">Copy to clipboard</button>
-            <textarea className="outputTextBox" type = "text" value={outputText} 
+            <button className="clear" onClick={handleClear}>Clear all text</button>
+            <button className="copy" onClick={handleCopy}>Copy to clipboard</button>
+            <textarea className="outputTextBox" type = "text" value={text}
                 readOnly placeholder='Output Text' rows={12} wrap='soft' cols={35}/>
             <div className='backspace'>
-                <button className="back">Backspace</button>
-                <button className="super-back">Super Backspace</button>
+                <button className="back" onClick={handleBackspace}>Backspace</button>
+                <button className="super-back" onClick={handleSuperBackspace}>Super Backspace</button>
             </div>
-            <button className="copy">Space</button>
+            <button className="copy" onClick={handleSpace}>Space</button>
         </div>
     );
 };
