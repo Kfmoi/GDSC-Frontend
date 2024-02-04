773cee6e4bffc2ccb87806fcfa44aed3ca3367ab
Hussain Muhammed <muhammeh@mcmaster.ca>
2024-02-04 01:55:03 -0500

    - editing look and feel

--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -6,18 +6,17 @@ import OutputPane from "../components/outputPane/OutputPane";
 import './FrontPage.css';
 const FrontPage = () => {
     return (
-        <div className="main-page">
-            <div className="title">
-                <h1>Text Editor</h1>
+            <div className="main-page">
+                <div className="grouper">
+                    <div className="function">
+                        <TextOutputDisplay />
+                        <TextInputDisplay />
+                    </div>
+                    <div className="outputBox">
+                        <OutputPane />
+                    </div>
+                </div>
             </div>
-            <div className="function">
-                <TextOutputDisplay />
-                <TextInputDisplay />
-            </div>
-            <div className="output">
-                <OutputPane />
-            </div>
-        </div>
-    );
+        );
     }
     export default FrontPage;
