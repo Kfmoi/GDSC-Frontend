5819bea62e9478a2f5afd21bad4bafb83d8fb0d0
Kfmoi <kfmoi@Kfmois-MacBook-Air.local>
2024-02-04 03:01:22 -0500

    Created the dropdown

--- src/components/textOutputDisplay/TextOutputDisplay.js
+++ src/components/textOutputDisplay/TextOutputDisplay.js
@@ -1,22 +1,33 @@
 import "./TextOutputDisplay.css";
+import React, { useState } from "react";
 const TextOutputDisplay = ({ onSave, text }) => {
   const handleCopy = () => {
     navigator.clipboard.writeText(text);
   };
+  const [selectedOption, setSelectedOption] = useState("");
   return (
     <div>
       <div className="output">
         <div className="column-display">
-          <input
+          <select
             className="input-text"
-            type="text"
-            value={text}
-            placeholder="Autocomplete Text"
-            readOnly
-          />
+            value={selectedOption}
+            onChange={(e) => setSelectedOption(e.target.value)}
+          >
+            <option value="Option 1">Option 1</option>
+            <option value="Option 2">Option 2</option>
+            <option value="Option 3">Option 3</option>
+          </select>
           <div className="output-buttons">
-            <button onClick={() => onSave(text)} className="output-button">Save</button>
-            <button onClick={handleCopy} className="output-button">Copy</button>
+            <button
+              onClick={() => onSave(selectedOption)}
+              className="output-button"
+            >
+              Save
+            </button>
+            <button onClick={handleCopy} className="output-button">
+              Copy
+            </button>
             <button className="output-button">C</button>
           </div>
         </div>
