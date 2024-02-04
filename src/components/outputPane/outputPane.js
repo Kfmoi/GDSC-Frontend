083951f7855662a0fbcac1405b3375e352b3a8a6
Hussain Muhammed <muhammeh@mcmaster.ca>
2024-02-04 02:13:05 -0500

    added functions for buttons

--- src/components/outputPane/outputPane.js
+++ src/components/outputPane/outputPane.js
@@ -3,12 +3,33 @@ import './OutputPane.css';
 
 const OutputPane = () => {
 
+    const [text, setText] = useState('');
+    
+    const handleClear = () => {
+        setText('');
+    };
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText(text);
+    }
+
+    const handleBackspace = () => {
+        setText(text.slice(0, text.length-1));
+    };
+
+    const handleSuperBackspace = () => {
+        // remove the last word
+    };
+
+    const handleSpace = () => {
+        setText(text + ' ');
+    }
 
     return (
         <div className="outputBox">
             <button className="clear">Clear all text</button>
             <button className="copy">Copy to clipboard</button>
-            <textarea className="outputTextBox" type = "text" value="" 
+            <textarea className="outputTextBox" type = "text" value={text} 
                 readOnly placeholder='Output Text' rows={12} wrap='soft' cols={35}/>
             <div className='backspace'>
                 <button className="back">Backspace</button>
